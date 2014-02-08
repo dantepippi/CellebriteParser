2ba98add7efb1bdd99bb84289833310880718713
Dante Filho <dante.dpf@gmail.com>
2014-02-08 00:10:06 +0000

    Alterando encoding para leitura do arquivo ASAP (LATIN-1)

--- preenche_laudo.py
+++ preenche_laudo.py
@@ -1,54 +1,58 @@
 import sys
 import zipfile
 import os
+import codecs
 from os.path import join
-
-asap = open(sys.argv[1], 'r')
-
 import ConfigParser
+
+tmp_dir = 'tmp/'
+file_path = '/home/dev/Dropbox/'
+
 def get_data_por_extenso(data):
     mes_ext = {1: 'janeiro', 2 : 'fevereiro', 3: 'marco', 4: 'abril', 5: 'maio', 6: 'junho', 7: 'julho', 8: 'agosto', 9: 'setembro', 10: 'outubro', 11: 'novembro', 12: 'dezembro'}
     dia, mes, ano = data.split("/")
     return dia + ' de ' + mes_ext[int(mes)] + ' de ' + ano
 
-
 def abreDoc(arquivo):
+    """
+    Abre o documento no diretorio temporario e retorna um array com o conteudo
+    dos dois arquivos de interesse (document e header)
+    """
     mydoc = zipfile.ZipFile(arquivo)
-    mydoc.extractall('/home/tmp/')
+    mydoc.extractall(tmp_dir)
     xmlcontent = [mydoc.read('word/document.xml'),  mydoc.read('word/header1.xml')]
     return xmlcontent
 
 def salva_documento(output):
-    os.chdir('/home/tmp/')
+    os.chdir(tmp_dir)
     os.walk('.')
     document = zipfile.ZipFile(
         output, mode='w', compression=zipfile.ZIP_DEFLATED)
     for dirpath, dirnames, filenames in os.walk('.'):
         for filename in filenames:
-            templatefile = join(dirpath, filename)
-            archivename = templatefile[2:]
-            document.write(templatefile, archivename)
+            nome_arquivo = join(dirpath, filename)
+            archivename = nome_arquivo[2:]
+            document.write(nome_arquivo, archivename)
+            os.remove(nome_arquivo)
     document.close()
 
 def replace(document, search, replace):
-    """
-    Replace all occurences of string with a different string, return updated
-    document
-    """
     newdocument = document.replace(search, replace)
     return newdocument    
     
-config = ConfigParser.ConfigParser()
-config.read(sys.argv[1])
+def abre_arquivo_conf():
+    config = ConfigParser.ConfigParser()
+    # Abre arquivo ASAP (que utiliza encoding LATIN-1)
+    config.readfp(codecs.open(sys.argv[1], "r", 'LATIN-1'))
+    return config
 
-file_path = '/home/dev/Dropbox/'
+
 document = abreDoc(file_path + 'd1.docx')
-newdocument = open('/home/tmp/word/document.xml', 'w')
-newheader = open('/home/tmp/word/header1.xml', 'w')
-
-
+newdocument = open(tmp_dir + 'word/document.xml', 'w')
+newheader = open(tmp_dir + 'word/header1.xml', 'w')
+# Substitui os valores do ASAP no documento
+config = abre_arquivo_conf()
 document[0] = replace(document[0], 'NUMLAUDO', config.get('LAUDO', 'NUMERO', 0))
-document[1] = replace(document[1], 'NUMLAUDO', config.get('LAUDO', 'NUMERO', 0))
 document[0] = replace(document[0], 'DATALAUDO', get_data_por_extenso(config.get('LAUDO', 'DATA', 0)))
 document[0] = replace(document[0], 'NOMEPERITO', config.get('LAUDO', 'PCF1', 0).split("|")[0])
 document[0] = replace(document[0], 'NUMIPL', config.get('LAUDO', 'NUMERO', 0))
@@ -57,13 +61,12 @@ document[0] = replace(document[0], 'DOCSOLICITANTE', config.get('SOLICITACAO', '
 document[0] = replace(document[0], 'DTDOC', config.get('SOLICITACAO', 'DATA_DOCUMENTO', 0))
 document[0] = replace(document[0], 'NUMSISCRIM', config.get('SOLICITACAO', 'NUMERO_CRIMINALISTICA', 0))
 document[0] = replace(document[0], 'DTSISCRIM', config.get('SOLICITACAO', 'DATA_CRIMINALISTICA', 0))
+
+# Substitui o numero do laudo no header
+document[1] = replace(document[1], 'NUMLAUDO', config.get('LAUDO', 'NUMERO', 0))
+
 newdocument.write(document[0])
 newheader.write(document[1])
 newdocument.close()
 newheader.close()
 salva_documento(file_path + 'd2.docx')
-
-
-
-
-
