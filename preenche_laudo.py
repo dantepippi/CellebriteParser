85f6f4e8cd57fa97ff652fa8a63dad18aebc8152
Dante Filho <dantepippi@gmail.com>
2014-02-10 22:05:22 +0000

    continuacao xml

--- preenche_laudo.py
+++ preenche_laudo.py
@@ -6,7 +6,7 @@ from os.path import join
 import ConfigParser
 from isomaker import gera_iso
 import glob
-import xmlparse
+from xmlparse import parse_arquivo_xml, replace
 
 tmp_dir = 'tmp/'
 file_path = sys.argv[1]
@@ -23,8 +23,8 @@ def abreDoc(arquivo):
     """
     mydoc = zipfile.ZipFile(arquivo)
     mydoc.extractall(tmp_dir)
-    xmlcontent = [mydoc.read('word/document.xml'),  mydoc.read('word/header1.xml')]
-    return xmlcontent
+    (document, header) = (mydoc.read('word/document.xml'),  mydoc.read('word/header1.xml'))
+    return (document, header)
 
 def salva_documento(output):
     os.chdir(tmp_dir)
@@ -38,10 +38,6 @@ def salva_documento(output):
             os.remove(nome_arquivo)
     document.close()
 
-def replace(document, search, replace):
-    newdocument = document.replace(search, replace)
-    return newdocument    
-    
 def abre_arquivo_conf():
     config = ConfigParser.ConfigParser()
     # Abre arquivo ASAP (que utiliza encoding LATIN-1)
@@ -49,33 +45,36 @@ def abre_arquivo_conf():
         config.readfp(codecs.open(arq, "r", 'LATIN-1'))
     return config
 
-def percorre_arquivos_xml():
-    for arq in glob.glob(file_path + "*.xml"):\
-        xmlparse.parse_arquivo_xml(arq)
+def percorre_arquivos_xml(document):
+    for arq in glob.glob(file_path + "*.xml"):
+        document = parse_arquivo_xml(arq, document)
+        #os.remove(arq)
+    return document
 
-document = abreDoc(file_path + 'd1.docx')
+(document, header) = abreDoc(file_path + 'd1.docx')
 newdocument = open(tmp_dir + 'word/document.xml', 'w')
 newheader = open(tmp_dir + 'word/header1.xml', 'w')
 # Substitui os valores do ASAP no documento
 config = abre_arquivo_conf()
+document = percorre_arquivos_xml(document)
 num_laudo = config.get('LAUDO', 'NUMERO', 0).encode('UTF-8')
 hash_iso = gera_iso(file_path, 'L'+ num_laudo.replace("/", "_"))
-document[0] = replace(document[0], 'NUMLAUDO', num_laudo)
-document[0] = replace(document[0], 'DATALAUDO', get_data_por_extenso(config.get('LAUDO', 'DATA', 0)))
-document[0] = replace(document[0], 'NOMEPERITO', config.get('LAUDO', 'PCF1', 0).split("|")[0])
-document[0] = replace(document[0], 'NUMIPL', config.get('SOLICITACAO', 'NUMERO_IPL', 0))
-document[0] = replace(document[0], 'IDAUTORIDADE', config.get('SOLICITACAO', 'AUTORIDADE', 0))
-document[0] = replace(document[0], 'DOCSOLICITANTE', config.get('SOLICITACAO', 'DOCUMENTO', 0))
-document[0] = replace(document[0], 'DTDOC', config.get('SOLICITACAO', 'DATA_DOCUMENTO', 0))
-document[0] = replace(document[0], 'NUMSISCRIM', config.get('SOLICITACAO', 'NUMERO_CRIMINALISTICA', 0))
-document[0] = replace(document[0], 'DTSISCRIM', config.get('SOLICITACAO', 'DATA_CRIMINALISTICA', 0))
-document[0] = replace(document[0], 'HASHISO', hash_iso.upper())
+document = replace(document, 'NUMLAUDO', num_laudo)
+document = replace(document, 'DATALAUDO', get_data_por_extenso(config.get('LAUDO', 'DATA', 0)))
+document = replace(document, 'NOMEPERITO', config.get('LAUDO', 'PCF1', 0).split("|")[0])
+document = replace(document, 'NUMIPL', config.get('SOLICITACAO', 'NUMERO_IPL', 0))
+document = replace(document, 'IDAUTORIDADE', config.get('SOLICITACAO', 'AUTORIDADE', 0))
+document = replace(document, 'DOCSOLICITANTE', config.get('SOLICITACAO', 'DOCUMENTO', 0))
+document = replace(document, 'DTDOC', config.get('SOLICITACAO', 'DATA_DOCUMENTO', 0))
+document = replace(document, 'NUMSISCRIM', config.get('SOLICITACAO', 'NUMERO_CRIMINALISTICA', 0))
+document = replace(document, 'DTSISCRIM', config.get('SOLICITACAO', 'DATA_CRIMINALISTICA', 0))
+document = replace(document, 'HASHISO', hash_iso.upper())
 
 # Substitui o numero do laudo no header
-document[1] = replace(document[1], 'NUMLAUDO', config.get('LAUDO', 'NUMERO', 0))
+header = replace(header, 'NUMLAUDO', config.get('LAUDO', 'NUMERO', 0))
 
-newdocument.write(document[0])
-newheader.write(document[1])
+newdocument.write(document)
+newheader.write(header)
 newdocument.close()
 newheader.close()
 salva_documento(file_path + 'd2.docx')
--- xmlparse.py
+++ xmlparse.py
@@ -1,17 +1,30 @@
+
 try:
     import xml.etree.cElementTree as ET
 except ImportError:
     import xml.etree.ElementTree as ET
 
+def replace(document, search, replace):
+    if replace is None:
+        return document
+    newdocument = document.replace(search, replace)
+    return newdocument
 
 def remove_espacos_exc(chunk):
     return  ' '.join(chunk.split())
 
-def parse_arquivo_xml(arquivo_xml):
+def parse_arquivo_xml(arquivo_xml, document):
     tree = ET.ElementTree(file=arquivo_xml)
     root = tree.getroot()
+    document = replace(document, 'MARCAMODELO', root.find('report/general_information/selected_manufacture'))
+    document = replace(document, 'NUM_IMEI1', root.find('report/general_information/imei'))
+    document = replace(document, 'DESC_OPERADORA', root.find('report/general_information/spn'))
+    document = replace(document, 'NUM_ICC', root.find('report/general_information/iccid'))
+    document = replace(document, 'NUM_IMSI', root.find('report/general_information/imsi'))    
+    
     for contato in tree.iter(tag='contact'):
         nome = remove_espacos_exc(contato.find('name').text)
         numero = contato.find('phone_number/value')
         print nome, '-', numero.text 
+    return document
 
