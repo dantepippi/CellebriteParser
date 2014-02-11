127398bf2b802b51f6c9eb4e0f3f617aa5969f3d
Dante Filho <dante.dpf@gmail.com>
2014-02-11 03:13:41 +0000

    adicionando varias secoes na tabela

--- preenche_laudo.py
+++ preenche_laudo.py
@@ -1,3 +1,4 @@
+import re
 import sys
 import zipfile
 import os
@@ -6,7 +7,7 @@ from os.path import join
 import ConfigParser
 from isomaker import gera_iso
 import glob
-from xmlparse import parse_arquivo_xml, replace
+from xmlparse import parse_arquivo_xml, replace_txt
 
 tmp_dir = 'tmp/'
 file_path = sys.argv[1]
@@ -41,37 +42,55 @@ def salva_documento(output):
 def abre_arquivo_conf():
     config = ConfigParser.ConfigParser()
     # Abre arquivo ASAP (que utiliza encoding LATIN-1)
-    for arq in glob.glob(file_path+"*.asap"):
+    for arq in glob.glob(file_path + "*.asap"):
         config.readfp(codecs.open(arq, "r", 'LATIN-1'))
     return config
 
-def percorre_arquivos_xml(document):
-    for arq in glob.glob(file_path + "*.xml"):
+def percorre_arquivos_xml(document, dir):
+    for arq in glob.glob(file_path + dir + "/*.xml"):
         document = parse_arquivo_xml(arq, document)
         #os.remove(arq)
     return document
 
+def get_lista_diretorios():
+    return sorted([name for name in os.listdir(file_path) if os.path.isdir(os.path.join(file_path, name))])
+
+def cria_secoes_tabela(document, lista_dirs):
+    cont = 0
+    for dir in lista_dirs:
+        cont += 1
+        conteudo_tabela = open('./template_tabela.txt').read()
+        document = insere_tabela(document, conteudo_tabela)
+        #document = replace_txt(document, 'CONT_TABELA' + str(cont), conteudo_tabela)
+        document = percorre_arquivos_xml(document, dir)
+    return document
+ 
+def insere_tabela(document, tabela):
+     posicao = re.search(r'<w:tbl', document).start()
+     return document[:posicao] + tabela + document[posicao:]
+     
+        
 (document, header) = abreDoc(file_path + 'd1.docx')
 newdocument = open(tmp_dir + 'word/document.xml', 'w')
 newheader = open(tmp_dir + 'word/header1.xml', 'w')
 # Substitui os valores do ASAP no documento
 config = abre_arquivo_conf()
-document = percorre_arquivos_xml(document)
+document = cria_secoes_tabela(document, get_lista_diretorios())
 num_laudo = config.get('LAUDO', 'NUMERO', 0).encode('UTF-8')
 hash_iso = gera_iso(file_path, 'L'+ num_laudo.replace("/", "_"))
-document = replace(document, 'NUMLAUDO', num_laudo)
-document = replace(document, 'DATALAUDO', get_data_por_extenso(config.get('LAUDO', 'DATA', 0)))
-document = replace(document, 'NOMEPERITO', config.get('LAUDO', 'PCF1', 0).split("|")[0])
-document = replace(document, 'NUMIPL', config.get('SOLICITACAO', 'NUMERO_IPL', 0))
-document = replace(document, 'IDAUTORIDADE', config.get('SOLICITACAO', 'AUTORIDADE', 0))
-document = replace(document, 'DOCSOLICITANTE', config.get('SOLICITACAO', 'DOCUMENTO', 0))
-document = replace(document, 'DTDOC', config.get('SOLICITACAO', 'DATA_DOCUMENTO', 0))
-document = replace(document, 'NUMSISCRIM', config.get('SOLICITACAO', 'NUMERO_CRIMINALISTICA', 0))
-document = replace(document, 'DTSISCRIM', config.get('SOLICITACAO', 'DATA_CRIMINALISTICA', 0))
-document = replace(document, 'HASHISO', hash_iso.upper())
+document = replace_txt(document, 'NUMLAUDO', num_laudo)
+document = replace_txt(document, 'DATALAUDO', get_data_por_extenso(config.get('LAUDO', 'DATA', 0)))
+document = replace_txt(document, 'NOMEPERITO', config.get('LAUDO', 'PCF1', 0).split("|")[0])
+document = replace_txt(document, 'NUMIPL', config.get('SOLICITACAO', 'NUMERO_IPL', 0))
+document = replace_txt(document, 'IDAUTORIDADE', config.get('SOLICITACAO', 'AUTORIDADE', 0))
+document = replace_txt(document, 'DOCSOLICITANTE', config.get('SOLICITACAO', 'DOCUMENTO', 0))
+document = replace_txt(document, 'DTDOC', config.get('SOLICITACAO', 'DATA_DOCUMENTO', 0))
+document = replace_txt(document, 'NUMSISCRIM', config.get('SOLICITACAO', 'NUMERO_CRIMINALISTICA', 0))
+document = replace_txt(document, 'DTSISCRIM', config.get('SOLICITACAO', 'DATA_CRIMINALISTICA', 0))
+document = replace_txt(document, 'HASHISO', hash_iso.upper())
 
 # Substitui o numero do laudo no header
-header = replace(header, 'NUMLAUDO', config.get('LAUDO', 'NUMERO', 0))
+header = replace_txt(header, 'NUMLAUDO', config.get('LAUDO', 'NUMERO', 0))
 
 newdocument.write(document)
 newheader.write(header)
