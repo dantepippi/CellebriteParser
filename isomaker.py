959da349730d01c6dea31256869ad4212d45a19b
Dante Filho <dantepippi@gmail.com>
2014-02-13 21:03:21 +0000

    separando o diretorio da midia

--- isomaker.py
+++ isomaker.py
@@ -4,32 +4,34 @@ import os
 from os.path import join
 
 tmp_dir = 'tmp/'
-pasta_teste = '/home/dev/Dropbox/'
 
-def gera_hashes(pasta):
+def grava_hashes(pasta):
     hashes = open(tmp_dir + 'hashes.txt', 'w')
     for dirpath, dirnames, filenames in os.walk(pasta):
         for filename in filenames:
             nome_completo = join(dirpath, filename)
-            hashes.write(filename + '=' + hashfile(nome_completo) + '\r\n')
+            hashes.write(calcula_hash(nome_completo) + ' ?SHA256*' + nome_completo.replace(pasta,'') + '\r\n')
     hashes.close()
     os.rename(tmp_dir + 'hashes.txt', pasta + 'hashes.txt')
     
 def gera_iso(pasta, label):
-    gera_hashes(pasta)
-    nome_iso = pasta_teste + label + '.iso'
+    grava_hashes(pasta)
+    nome_iso = pasta + 'resultado' + label + '.iso'
     p = subprocess.Popen(['mkisofs', '-J', '-l', '-R', '-V', label, '-iso-level', '4', '-o', nome_iso, pasta])
     p.communicate()
     os.remove(pasta + 'hashes.txt')
-    return hashfile(nome_iso)
+    return calcula_hash(nome_iso)
 
-def hashfile(filename, hasher=hashlib.sha256(), blocksize=65536):
+def calcula_hash(filename, blocksize=65536):
+    hasher = hashlib.sha256()
     afile = open(filename, 'rb')
     buf = afile.read(blocksize)
     while len(buf) > 0:
         hasher.update(buf)
         buf = afile.read(blocksize)
-    return hasher.hexdigest()
+    str_hash = hasher.hexdigest()
+    afile.close()
+    return str_hash
 
-#gera_hashes(pasta_teste)
+#grava_hashes(pasta_teste)
 #print gera_iso(pasta_teste, 'ISO') 
--- preenche_laudo.py
+++ preenche_laudo.py
@@ -7,10 +7,12 @@ from os.path import join
 import ConfigParser
 from isomaker import gera_iso
 import glob
+import shutil
 from xmlparse import parse_arquivo_xml, replace_txt
 
 tmp_dir = 'tmp/'
-file_path = sys.argv[1]
+file_path_base = sys.argv[1]
+file_path_midia = file_path_base + 'midia/'
 
 def get_data_por_extenso(data):
     mes_ext = {1: 'janeiro', 2 : 'fevereiro', 3: 'marco', 4: 'abril', 5: 'maio', 6: 'junho', 7: 'julho', 8: 'agosto', 9: 'setembro', 10: 'outubro', 11: 'novembro', 12: 'dezembro'}
@@ -42,18 +44,22 @@ def salva_documento(output):
 def abre_arquivo_conf():
     config = ConfigParser.ConfigParser()
     # Abre arquivo ASAP (que utiliza encoding LATIN-1)
-    for arq in glob.glob(file_path + "*.asap"):
+    for arq in glob.glob(file_path_base + "*.asap"):
         config.readfp(codecs.open(arq, "r", 'LATIN-1'))
     return config
 
 def percorre_arquivos_xml(document, diretorio):
-    for arq in glob.glob(file_path + diretorio + "/*.xml"):
+    for arq in glob.glob(file_path_midia + diretorio + "/*.xml"):
         document = parse_arquivo_xml(arq, document)
-        #os.remove(arq) 
+        os.remove(arq) 
     return document
 
+def copia_arquivo_imagem():
+    for arq in glob.glob(file_path_base + "*.png"):
+        shutil.copyfile(arq, tmp_dir + 'word/media/image2.png')
+
 def get_lista_diretorios():
-    return sorted([name for name in os.listdir(file_path) if os.path.isdir(os.path.join(file_path, name))])
+    return sorted([name for name in os.listdir(file_path_midia) if os.path.isdir(os.path.join(file_path_midia, name))])
 
 def cria_secoes_tabela(document, lista_dirs):
     for diretorio in lista_dirs:
@@ -66,16 +72,20 @@ def cria_secoes_tabela(document, lista_dirs):
 def insere_tabela(document, tabela):
     posicao = re.search(r'<w:p w:rsidR="00F00A10" w:rsidRDefault="00FA64C5"><w:pPr><w:pStyle w:val="LLeg"/><w:jc w:val="both"/></w:pPr><w:r><w:rPr><w:b/><w:iCs/><w:spacing w:val="-3"/></w:rPr><w:t>Legenda:</w:t>', document).start()
     return document[:posicao] + tabela + document[posicao:]
-     
+   
+def backup_arquivos_midia():
+    shutil.copytree(file_path_midia, file_path_base + 'midia2')  
         
-(document, header) = abreDoc(file_path + 'd1.docx')
+(document, header) = abreDoc(file_path_base + 'd1.docx')
 newdocument = open(tmp_dir + 'word/document.xml', 'w')
 newheader = open(tmp_dir + 'word/header1.xml', 'w')
 # Substitui os valores do ASAP no documento
 config = abre_arquivo_conf()
+copia_arquivo_imagem()
+backup_arquivos_midia()
 document = cria_secoes_tabela(document, get_lista_diretorios())
 num_laudo = config.get('LAUDO', 'NUMERO', 0).encode('UTF-8')
-hash_iso = gera_iso(file_path, 'L'+ num_laudo.replace("/", "_"))
+hash_iso = gera_iso(file_path_midia, 'L'+ num_laudo.replace("/", "_"))
 document = replace_txt(document, 'NUMLAUDO', num_laudo)
 document = replace_txt(document, 'DATALAUDO', get_data_por_extenso(config.get('LAUDO', 'DATA', 0)))
 document = replace_txt(document, 'NOMEPERITO', config.get('LAUDO', 'PCF1', 0).split("|")[0])
@@ -94,4 +104,4 @@ newdocument.write(document)
 newheader.write(header)
 newdocument.close()
 newheader.close()
-salva_documento(file_path + 'd2.docx')
+salva_documento(file_path_base + 'd2.docx')
