0646bddde830b0c21fcb21c8b6b6ebd261c5bdac
Dante Filho <dante.dpf@gmail.com>
2014-02-19 13:48:24 +0000

    codigo dos contatos no html

--- xmlparse.py
+++ xmlparse.py
@@ -12,9 +12,17 @@ def replace_txt(conteudo, search, replace_txt):
 def remove_espacos_exc(chunk):
     return  ' '.join(chunk.split())
 
+def adiciona_contatos(tree, diretorio):
+    contacts = ''
+    for contato in tree.iter(tag='contact'):
+        nome = remove_espacos_exc(contato.find('name').text)
+        numero = contato.find('phone_number/value')
+        txt_contatos = '<tr><td class="origem">' + diretorio + '</td><td class="numero">' + numero + '</td><td class="nome">' + nome + '</td> </tr>'
+
 def parse_arquivo_xml(arquivo_xml, conteudo, diretorio):
     tree = ET.ElementTree(file=arquivo_xml)
     root = tree.getroot()
+    adiciona_contatos(diretorio, tree)
     conteudo = replace_txt(conteudo, 'ID_CEL', diretorio)
     if root.find('report/general_information/report_type').text == 'cell':
         conteudo = replace_txt(conteudo, 'MARCAMODELO', root.find('report/general_information/selected_manufacture').text + ' ' + root.find('report/general_information/detected_model').text )
@@ -23,11 +31,5 @@ def parse_arquivo_xml(arquivo_xml, conteudo, diretorio):
         conteudo = replace_txt(conteudo, 'DESC_OPERADORA', root.find('report/general_information/spn').text)
         conteudo = replace_txt(conteudo, 'NUM_ICC', root.find('report/general_information/iccid').text)
         conteudo = replace_txt(conteudo, 'NUM_IMSI', root.find('report/general_information/imsi').text)    
-    """
-    for contato in tree.iter(tag='contact'):
-        nome = remove_espacos_exc(contato.find('name').text)
-        numero = contato.find('phone_number/value')
-        print nome, '-', numero.text 
-    """
     return conteudo
 
